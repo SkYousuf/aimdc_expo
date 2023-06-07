7141a247f174063ecbcc900304bf46afcc702642
Abdul Muqtadir <abdulmuq.dev@gmail.com>
2023-06-07 16:54:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import YourComp from "./YourComp"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <YourComp/>
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/YourComp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import css here
+
+function YourComp() {
+  return (
+    <div>
+      <h1>HELLO</h1>
+    </div>
+  )
+}
+
+export default YourComp
